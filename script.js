f4b006469840aecdb05738276f09da942934dffe
Ryan Macalandag <rmacalandag@gmail.com>
2024-02-07 00:06:42 +1100

    settings, dark mode

---
+++ script.js
@@ -0,0 +1,13 @@
+const cog = document.querySelector('.cog-button');
+const drawer = document.querySelector('#drawer');
+const darkToggle = document.querySelector('.dark-toggle');
+const body = document.querySelector('body');
+
+cog.addEventListener('click', (e) => {
+  drawer.classList.toggle('open');
+});
+
+darkToggle.addEventListener('click', (e) => {
+  body.classList.toggle('dark');
+})
+
